d0a0590c68b8bc5785c85c0bbc3bb0b1cca87be0
jjaspe <jjaspe09@hotmail.com>
2014-11-17 16:37:16 -0500

    Added Enums file

---
+++ SneakingCommon/Interfaces/Model Interfaces/IGameMaster.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SneakingCommon.Model_Stuff;
+using Canvas_Window_Template.Basic_Drawing_Functions;
+using Canvas_Window_Template.Interfaces;           
+using Canvas_Window_Template.Drawables;
+using SneakingCommon.System_Classes;
+using SneakingCommon.Data_Classes;
+using SneakingCommon.Interfaces.Model;
+using SneakingCommon.Interfaces.Observer_Pattern;
+
+namespace SneakingCommon.Interfaces.Model
+{
+    public interface IGameMaster:IModelSubject
+    {
+        NoiseMap getNoiseMap();
+        void setNoiseMap(NoiseMap nm);
+        void setGuards(List<Guard> guards);
+        List<IGuard> getGuards();
+        void setPC(PC pc);
+        IGuard getPC();
+        void setMap(Map map);
+        Map getMap();
+        IGuard getActiveGuard();
+        void sortGuards(string statName);
+        List<IGuard> getSortedGuards();
+        void commitChanges();
+        bool tileFreeFromGuards(IPoint tileOrigin);
+        bool GameStarted { get; set; }
+    }
+}
---
+++ SneakingCommon/System Classes/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SneakingCommon.System_Classes
+{
+    public enum GoalName {ReachTile,
+                           ReachTileAndAction,
+                            ReachTileAndActionAndTile,
+                            Guards,
+                           SpecialGuard};
+}
